db72c49458a3f8ddd050223f8a7f732bdda168ca
jiaSongLin <844030491@qq.com>
2018-06-21 20:18:48 +0800

    添加全局$echarts对象
    在this.$echarts直接使用

--- src/main.js
+++ src/main.js
@@ -4,7 +4,10 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+const echarts = require('echarts')
+
 Vue.config.productionTip = false
+Vue.prototype.$echarts = echarts
 
 /* eslint-disable no-new */
 new Vue({
--- src/pages/tempBoard/index.vue
+++ src/pages/tempBoard/index.vue
@@ -1,15 +1,48 @@
 <template>
   <div>
-    texnasldiufhjudysjfghiawelrgfsdk
+    <div
+      id="echart1"
+      ref="echart1"
+      class="echart"></div>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+    }
+  },
+  methods: {
+    render () {
+      const option = {
+        xAxis: {
+          type: 'category',
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          data: [820, 932, 901, 934, 1290, 1330, 1320],
+          type: 'line'
+        }]
+      }
+      const dom = this.$refs.echart1
+      console.log(dom)
+      const echart = this.$echarts.init(dom)
+      echart.setOption(option)
+    }
+  },
+  mounted: function () {
+    this.render()
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.echart {
+  height: 500px;
+  width: 700px;
+}
 </style>
